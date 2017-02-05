3e5109319ccd050935d7329c7fd770be9d84c64c
Federico <fede6993@hotmail.it>
2017-02-05 13:28:27 -0800

    refactored with partials. added desotry commnets

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,11 @@
 class Article < ApplicationRecord
   # relationship with the comments table
   # now there is an array in the object @article called @article.comments that retrives all the comments of this article
-  has_many :comments
+
+  # If you delete an article, its associated comments will also need to be deleted,
+  # otherwise they would simply occupy space in the database. Rails allows you to use
+  # the dependent option of an association to achieve this.
+  has_many :comments, dependent: :destroy
 
   # in the model you can add data validation before going into the database
   validates :title, presence: true,
